bef9438bfdc1e404305506e6f6676fb9eb7f6141
Felipe Bryan <felipe.bryan@live.com>
2025-02-22 18:53:46 -0300

    feat: updates

---
+++ perdas/src/functions/reloadCount.ts
@@ -0,0 +1,38 @@
+import { dateString } from '../utils/handleDate';
+import { getStorageData, saveToStorage } from '../utils/storageFunctions';
+
+interface ReloadProps {
+  date1: string;
+  date2: string;
+}
+
+const storageKey: string = 'reloads-perdas';
+
+export function reloadCount() {
+  const reloads: ReloadProps = getStorageData(storageKey);
+
+  const today = dateString();
+
+  let current: ReloadProps = {
+    date1: today,
+    date2: '-',
+  };
+
+  if (!reloads) {
+    saveToStorage(storageKey, current);
+
+    location.reload();
+  } else {
+    if (reloads.date1 != today) {
+      saveToStorage(storageKey, current);
+
+      location.reload();
+    } else if (reloads.date2 != today) {
+      reloads.date2 = today;
+
+      saveToStorage(storageKey, reloads);
+
+      location.reload();
+    }
+  }
+}
--- perdas/src/startApp.ts
+++ perdas/src/startApp.ts
@@ -1,9 +1,12 @@
 import { checkOrder } from './functions/checkOrder';
+import { reloadCount } from './functions/reloadCount';
 import { sendOrder } from './functions/sendOrder';
 import { startCart } from './pages/cart/startCart';
 import { startHome } from './pages/home/startHome';
 
 export function startApp() {
+  reloadCount();
+
   checkOrder();
 
   startHome();
